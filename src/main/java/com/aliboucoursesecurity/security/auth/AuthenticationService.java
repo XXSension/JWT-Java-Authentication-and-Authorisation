63665b53595daa80f8b631e4945abbff76a95ab6
apmarev <ismailov.timur2011@yandex.ru>
2023-02-15 18:02:25 +0300

    first commit

---
+++ src/main/java/com/aliboucoursesecurity/security/auth/AuthenticationService.java
@@ -0,0 +1,57 @@
+package com.aliboucoursesecurity.security.auth;
+
+import com.aliboucoursesecurity.security.config.JwtService;
+import com.aliboucoursesecurity.security.user.Role;
+import com.aliboucoursesecurity.security.user.User;
+import com.aliboucoursesecurity.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authentication(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found "));
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+
+}
---
+++ src/main/java/com/aliboucoursesecurity/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.aliboucoursesecurity.security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
